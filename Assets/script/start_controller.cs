6e2ad8dc1df4e81b72ff9d8be5a18d04972c6775
Chas <tangchao3@outlook.com>
2017-05-14 19:54:15 +0800

    comit
    tes

---
+++ Assets/script/start_controller.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class start_controller : MonoBehaviour {
+
+	void Start()
+	{
+		Screen.SetResolution(480, 800, false);
+	}
+
+
+	public void OnStartButtonClick()
+	{
+		SceneManager.LoadScene("game_01");
+	}
+
+
+	public void OnQuitButtonClick()
+	{
+		Application.Quit();
+	}
+}
